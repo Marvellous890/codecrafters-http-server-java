45811c53aa2b1f143353e9a2c9edd9fcfbba77ea
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2024-07-18 09:43:50 +0100

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
@@ -13,8 +14,17 @@ public class Main {
 
       Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
       System.out.println("accepted new connection");
-
-      clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      InputStream input = clientSocket.getInputStream();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+      String line = reader.readLine();
+      System.out.println(line);
+      String[] HttpRequest = line.split(" ", 0);
+      OutputStream output = clientSocket.getOutputStream();
+      if (HttpRequest[1].equals("/")) {
+        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      } else {
+        output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+      }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
