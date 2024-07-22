3befd778faf9a52f208aba474278ebd1b22f2c68
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2024-07-22 12:36:38 +0100

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
+import java.util.Locale;
 
 public class Main {
   public static void main(String[] args) {
@@ -17,7 +17,7 @@ public class Main {
       InputStream input = clientSocket.getInputStream();
       BufferedReader reader = new BufferedReader(new InputStreamReader(input));
       String line = reader.readLine();
-      System.out.println(line);
+//      System.out.println(line);
       String[] HttpRequest = line.split(" ", 0);
       OutputStream output = clientSocket.getOutputStream();
       if (HttpRequest[1].equals("/")) {
@@ -26,11 +26,30 @@ public class Main {
         String content = HttpRequest[1].replace("/echo/", "");
         String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + content.length() + "\r\n\r\n" + content;
         output.write(response.getBytes());
+      } else if (HttpRequest[1].equals("/user-agent")) {
+        boolean uaFound = false;
+        while (!uaFound) {
+          String nextLine = reader.readLine();
+          if (nextLine.toLowerCase().startsWith("user-agent")) {
+            uaFound = true;
+            line = nextLine;
+          }
+        }
+
+        line = line.toLowerCase().replace("user-agent: ", "");
+        String resp = prepareHeaders(line.length()) + line;
+        System.out.println(resp);
+        output.write(resp.getBytes());
+
       } else {
         output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
+  }
+
+  private static String prepareHeaders(int contentLength) {
+    return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + contentLength + "\r\n\r\n";
   }
 }
